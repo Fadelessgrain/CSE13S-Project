5e22dfd40185b81a3230bbc73c925441cecadeac
kathy <cduarter@ucsc.edu>
2021-01-14 13:22:09 -0800

    Added my Makefile, names.h, and vampire.c

---
+++ asgn1/vampire.c
@@ -0,0 +1,32 @@
+#include "names.h"
+#include <stdio.h>
+#include <inttypes.h>
+#include <stdlib.h>
+
+int lives[10] = { 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 };
+
+int main(void) {
+	int players = 0;
+	printf("Number of players: ");
+	scanf("%d" , &players); 
+	if (players < 2 || players > 10) { 
+		fprintf(stderr, "Invalid number of players.\n");                                               
+         exit(1);
+	}
+	else {
+	  int64_t seed = 0;                                                                               
+       printf("Random seed: ");                                                                        
+       if (scanf("%" SCNd64, &seed) != 1 || seed < 0){                                                 
+           fprintf(stderr, "Invalid random seed\n");                                                               }                                                   
+      if (seed > UINT32_MAX){                                                                    
+              fprintf(stderr, "Invalid random seed\n");                                                
+             exit(1);    
+
+	  }
+	}
+
+	for ( int i = 1; i < 10; i += 1) {
+		printf("Round %d\n", i);
+	}
+return 0;
+}
