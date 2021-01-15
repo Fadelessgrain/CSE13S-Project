36870bec34e60c640f32a321c686a528b8cdaeb2
kathy <cduarter@ucsc.edu>
2021-01-14 21:16:12 -0800

    started my loop but random is not working

--- asgn1/vampire.c
+++ asgn1/vampire.c
@@ -3,30 +3,41 @@
 #include <inttypes.h>
 #include <stdlib.h>
 
-int lives[10] = { 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 };
+int roll(void) {
+	return rand() % 6;
+ }
 
+int lives[10] = { 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 };
 int main(void) {
 	int players = 0;
 	printf("Number of players: ");
 	scanf("%d" , &players); 
 	if (players < 2 || players > 10) { 
-		fprintf(stderr, "Invalid number of players.\n");                                               
-         exit(1);
+		fprintf(stderr, "Invalid number of players.\n");
+		exit(1);
 	}
 	else {
-	  int64_t seed = 0;                                                                               
-       printf("Random seed: ");                                                                        
-       if (scanf("%" SCNd64, &seed) != 1 || seed < 0){                                                 
-           fprintf(stderr, "Invalid random seed\n");                                                               }                                                   
-      if (seed > UINT32_MAX){                                                                    
-              fprintf(stderr, "Invalid random seed\n");                                                
-             exit(1);    
-
+	  int64_t seed = 0;     
+	  printf("Random seed: ");
+	  if (scanf("%" SCNd64, &seed) != 1 || seed < 0){  
+	  	fprintf(stderr, "Invalid random seed\n");    
 	  }
-	}
-
-	for ( int i = 1; i < 10; i += 1) {
+	  if (seed > UINT32_MAX){  
+	  	fprintf(stderr, "Invalid random seed\n");                   
+       	exit(1);    
+	  }else{
+//	for ( int i = 1; i < 10 ; i += 1) {
+		for (int i = 1; i < lives[i]; i +=1) {
 		printf("Round %d\n", i);
-	}
-return 0;
+		srand(seed);
+		for (int s = 0; s < players; s+= 1){
+			int r = roll();
+			printf(" - %s rolls %s\n",names[s], rolls[r][r]);
+	
 }
+}
+	}
+	return 0;
+}
+}
+
