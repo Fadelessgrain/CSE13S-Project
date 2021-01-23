16e20b637536613b1130e6718de0bf749befa8e4
kathy <cduarter@ucsc.edu>
2021-01-22 20:57:28 -0800

    Clang format

--- asgn2/mathlib-test.c
+++ asgn2/mathlib-test.c
@@ -1,107 +1,86 @@
 #include "mathlib.h"
+
 #include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h>
 
-
-#define OPTIONS  "asctel"
+#define OPTIONS "asctel"
 
 int main(int argc, char **argv) {
-	int opt = 0;
-	bool return_sin, return_cos, return_tan, return_exp, return_log;
-	while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
-	switch (opt) {
-	case 'a': 
-		return_sin = true;
-		return_cos = true;
-		return_tan = true;
-		return_exp = true; 
-		return_log = true;
-		break;
-	case 's':             
-		return_sin = true;
-         break;
-	case 'c':
-		return_cos = true;
-         break;
-	case 't':
-		return_tan = true;
-         break;
-	case 'e':     
-		return_exp = true;
-        break;
-	case 'l':
-		return_log = true;
-         break;
-	default: 
-	fprintf(stderr, "Not a valid option. Use %s -[asctel]\n", argv[0]);
-		}
-	}
-	if (return_sin == true ) {
-		printf("  x           Cos              Library        Difference\n");
-		printf("  -           ---               -------        ---------\n");
-		for (double x = -2*M_PI; x < 2 *M_PI; x+= 0.1) {             
-				double my_sin = Sin(x);
-				double library_sin = sin(x);
-				double diff = my_sin - library_sin;
-				printf("%7.4f % 16.8f % 16.8f % 16.10f\n", x, my_sin, 
-				library_sin, diff);
-		}
-	}
-	
-	if (return_cos == true ) {
-           printf("  x           Cos              Library        Difference\n");
-           printf("  -           ---    	       -------        ---------\n");
-           for (double x = -2*M_PI; x < 2 *M_PI; x+= 0.1) {
-                   double my_cos = Cos(x);
-                   double library_cos = cos(x);
-                   double diffe = my_cos - library_cos;
-                   printf("%7.4f % 16.8f % 16.8f % 16.10f\n", 
-  					x, my_cos,library_cos, diffe);
-           }
-  }
-
-  if (return_tan == true ) {
-              printf("  x           Tan              Library        Difference\n");
-              printf("  -           ---               -------        ---------\n");
-              for (double x = -1*(M_PI/3); x < (M_PI/3); x+= 0.1) {
-                      double my_tan = Tan(x);
-                      double library_tan = tan(x);
-                      double diffe = my_tan - library_tan;
-                      printf("%7.4f % 16.8f % 16.8f % 16.10f\n",
-                       x, my_tan,library_tan, diffe);
-             }
+    int opt = 0;
+    bool return_sin, return_cos, return_tan, return_exp, return_log;
+    while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+        switch (opt) {
+        case 'a':
+            return_sin = true;
+            return_cos = true;
+            return_tan = true;
+            return_exp = true;
+            return_log = true;
+            break;
+        case 's': return_sin = true; break;
+        case 'c': return_cos = true; break;
+        case 't': return_tan = true; break;
+        case 'e': return_exp = true; break;
+        case 'l': return_log = true; break;
+        default: fprintf(stderr, "Not a valid option. Use %s -[asctel]\n", argv[0]);
+        }
+    }
+    if (return_sin == true) {
+        printf("  x           Cos              Library        Difference\n");
+        printf("  -           ---               -------        ---------\n");
+        for (double x = -2 * M_PI; x < 2 * M_PI; x += 0.1) {
+            double my_sin = Sin(x);
+            double library_sin = sin(x);
+            double diff = my_sin - library_sin;
+            printf("%7.4f % 16.8f % 16.8f % 16.10f\n", x, my_sin, library_sin, diff);
+        }
     }
 
-if (return_exp == true ) {
-              printf("  x           Exp              Library        Difference\n");
-              printf("  -           ---               -------        ---------\n");
-              for (double x = 1; x <= 10; x+= 0.1) {
-                      double my_exp = Exp(x);
-                      double library_exp = exp(x);
-                      double diffe = my_exp - library_exp;
-                      printf("%7.4f % 16.8f % 16.8f % 16.10f\n",
-                       x, my_exp,library_exp, diffe);
-             }
+    if (return_cos == true) {
+        printf("  x           Cos              Library        Difference\n");
+        printf("  -           ---    	       -------        ---------\n");
+        for (double x = -2 * M_PI; x < 2 * M_PI; x += 0.1) {
+            double my_cos = Cos(x);
+            double library_cos = cos(x);
+            double diffe = my_cos - library_cos;
+            printf("%7.4f % 16.8f % 16.8f % 16.10f\n", x, my_cos, library_cos, diffe);
+        }
+    }
 
+    if (return_tan == true) {
+        printf("  x           Tan              Library        Difference\n");
+        printf("  -           ---               -------        ---------\n");
+        for (double x = -1 * (M_PI / 3); x < (M_PI / 3); x += 0.1) {
+            double my_tan = Tan(x);
+            double library_tan = tan(x);
+            double diffe = my_tan - library_tan;
+            printf("%7.4f % 16.8f % 16.8f % 16.10f\n", x, my_tan, library_tan, diffe);
+        }
+    }
 
-}
+    if (return_exp == true) {
+        printf("  x           Exp              Library        Difference\n");
+        printf("  -           ---               -------        ---------\n");
+        for (double x = 1; x <= 10; x += 0.1) {
+            double my_exp = Exp(x);
+            double library_exp = exp(x);
+            double diffe = my_exp - library_exp;
+            printf("%7.4f % 16.8f % 16.8f % 16.10f\n", x, my_exp, library_exp, diffe);
+        }
+    }
 
-if (return_log == true ) {
-                printf("  x           Log              Library        Difference\n");
-                printf("  -           ---               -------        ---------\n");
-                for (double x = 1; x <= 10; x+= 0.1) {
-                        double my_log = Log(x);
-                         double library_log = log(x);
-                         double diffe = my_log - library_log;
-                         printf("%7.4f % 16.8f % 16.8f % 16.10f\n",
-                          x, my_log,library_log, diffe);
-                }
+    if (return_log == true) {
+        printf("  x           Log              Library        Difference\n");
+        printf("  -           ---               -------        ---------\n");
+        for (double x = 1; x <= 10; x += 0.1) {
+            double my_log = Log(x);
+            double library_log = log(x);
+            double diffe = my_log - library_log;
+            printf("%7.4f % 16.8f % 16.8f % 16.10f\n", x, my_log, library_log, diffe);
+        }
+    }
 
-  
-  }
-
-
-return 0;
+    return 0;
 }
--- asgn2/mathlib.c
+++ asgn2/mathlib.c
@@ -1,71 +1,68 @@
-#include "mathlib.h" 
-#include <stdio.h>
+#include "mathlib.h"
 
+#include <stdio.h>
 
 #define EPSILON 1e-14
 
-static inline double Abs(double x ) {
-	return x < 0 ? -x : x;
+static inline double Abs(double x) {
+    return x < 0 ? -x : x;
 }
 
-
-double Sin(double x) { 
-	double num = x;
-	double den = 1.0;
-	double sum = num/den;
-	double term = num/den;
-//	int factorial_ = 3.0;
-	for (double factorial = 3.0; Abs(term) > EPSILON; factorial += 2.0) {
-		double  mul = -x * x;
-		num = num * mul; 
-		den = den * (factorial-1) * factorial;
-		term = num/den;
-		sum = sum + term;
-	}
-	return sum; 
+double Sin(double x) {
+    double num = x;
+    double den = 1.0;
+    double sum = num / den;
+    double term = num / den;
+    //	int factorial_ = 3.0;
+    for (double factorial = 3.0; Abs(term) > EPSILON; factorial += 2.0) {
+        double mul = -x * x;
+        num = num * mul;
+        den = den * (factorial - 1) * factorial;
+        term = num / den;
+        sum = sum + term;
+    }
+    return sum;
 }
 
- double Cos(double x) {
-       double num = 1.0;
-      double den = 1.0;
-       double sum = num/den;
-       double term = num/den;
-       for (double factorial = 2.0; Abs(term) > EPSILON; factorial += 2.0) {
-           double  mul = -x * x;
-   	       num = num * mul;
-           den = den * (factorial-1)* factorial ;
-           term = num/den;
-          sum = sum + term;
-      }
-      return sum;
- }
+double Cos(double x) {
+    double num = 1.0;
+    double den = 1.0;
+    double sum = num / den;
+    double term = num / den;
+    for (double factorial = 2.0; Abs(term) > EPSILON; factorial += 2.0) {
+        double mul = -x * x;
+        num = num * mul;
+        den = den * (factorial - 1) * factorial;
+        term = num / den;
+        sum = sum + term;
+    }
+    return sum;
+}
 
- double Tan(double x) {
- 	double num = Sin(x);
-	double den = Cos(x);
- 	return num/den;
+double Tan(double x) {
+    double num = Sin(x);
+    double den = Cos(x);
+    return num / den;
 }
 
 double Exp(double x) {
-	double num = 1.0;
-	double den = 1.0;
-	double term = num/den;
-	double sum = term;
-	for (double factorial = 1.0; Abs(term) > EPSILON; factorial += 1.0) {
-		term = (x / factorial) * term;
-		sum = sum + term;
-		
-	}
-		return sum;
-	
+    double num = 1.0;
+    double den = 1.0;
+    double term = num / den;
+    double sum = term;
+    for (double factorial = 1.0; Abs(term) > EPSILON; factorial += 1.0) {
+        term = (x / factorial) * term;
+        sum = sum + term;
+    }
+    return sum;
 }
 
 double Log(double x) {
-	double ex = 1.0;
-	double power = Exp(ex);
-	while (Abs(x-power) > EPSILON) {
-		ex = ex + ((x-power) / power);
-		power = Exp(ex);
-		}
-	return ex;
-	}
+    double ex = 1.0;
+    double power = Exp(ex);
+    while (Abs(x - power) > EPSILON) {
+        ex = ex + ((x - power) / power);
+        power = Exp(ex);
+    }
+    return ex;
+}
