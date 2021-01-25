af368283196eae754ab895d67adcc45abe98f08c
kathy <cduarter@ucsc.edu>
2021-01-24 20:36:19 -0800

    scan-build bug found and fixed

--- asgn2/mathlib-test.c
+++ asgn2/mathlib-test.c
@@ -10,26 +10,26 @@
 
 int main(int argc, char **argv) {
     int opt = 0;
-    bool return_sin, return_cos, return_tan, return_exp, return_log;
+    bool r_sin, r_cos, r_tan, r_exp, r_log;
     while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
         switch (opt) {
         case 'a':
-            return_sin = true;
-            return_cos = true;
-            return_tan = true;
-            return_exp = true;
-            return_log = true;
+            r_sin = true;
+            r_cos = true;
+            r_tan = true;
+            r_exp = true;
+            r_log = true;
             break;
-        case 's': return_sin = true; break;
-        case 'c': return_cos = true; break;
-        case 't': return_tan = true; break;
-        case 'e': return_exp = true; break;
-        case 'l': return_log = true; break;
+        case 's': r_sin = true; break;
+        case 'c': r_cos = true; break;
+        case 't': r_tan = true; break;
+        case 'e': r_exp = true; break;
+        case 'l': r_log = true; break;
         default: fprintf(stderr, "Not a valid option. Use %s -[asctel]\n", argv[0]);
         }
     }
     //code above inspired by Eugene's lab
-    if (return_sin == true) {
+    if (r_sin == true) {
         printf("  x           Cos              Library        Difference\n");
         printf("  -           ---               -------        ---------\n");
         for (double x = -2 * M_PI; x < 2 * M_PI; x += 0.1) {
@@ -39,8 +39,9 @@ int main(int argc, char **argv) {
             printf("%7.4f % 16.8f % 16.8f % 16.10f\n", x, my_sin, library_sin, diff);
         }
     }
+    
 
-    if (return_cos == true) {
+    if (r_cos == true) {
         printf("  x           Cos              Library        Difference\n");
         printf("  -           ---    	       -------        ---------\n");
         for (double x = -2 * M_PI; x < 2 * M_PI; x += 0.1) {
@@ -51,7 +52,7 @@ int main(int argc, char **argv) {
         }
     }
 
-    if (return_tan == true) {
+    if (r_tan == true) {
         printf("  x           Tan              Library        Difference\n");
         printf("  -           ---               -------        ---------\n");
         for (double x = -1 * (M_PI / 3); x < (M_PI / 3); x += 0.1) {
@@ -62,7 +63,7 @@ int main(int argc, char **argv) {
         }
     }
 
-    if (return_exp == true) {
+    if (r_exp == true) {
         printf("  x           Exp              Library        Difference\n");
         printf("  -           ---               -------        ---------\n");
         for (double x = 1; x <= 10; x += 0.1) {
@@ -73,7 +74,7 @@ int main(int argc, char **argv) {
         }
     }
 
-    if (return_log == true) {
+    if (r_log == true) {
         printf("  x           Log              Library        Difference\n");
         printf("  -           ---               -------        ---------\n");
         for (double x = 1; x <= 10; x += 0.1) {
