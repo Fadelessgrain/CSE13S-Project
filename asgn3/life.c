0b496c19cc4439b29da7e891e6486172b76df297
kathy <cduarter@ucsc.edu>
2021-01-31 19:24:31 -0800

    wrong output but no big errors

--- asgn3/life.c
+++ asgn3/life.c
@@ -4,96 +4,134 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+//creates the command line opntions
 #define OPTIONS "tsn:i:o:"
-#define DELAY   50000
+//sets the delay
+#define DELAY 50000
 
 int main(int argc, char **argv) {
     int rows = 0;
     int cols = 0;
     int opt = 0;
+    //set the standard file
     FILE *infile = stdin;
+    //set the standard file to print to
     FILE *outfile = stdout;
-
+    //set the stanard geneation to 100
     int generation = 100;
-
+    //init toroidal
     bool toroidal = true;
     bool r_s = true;
     while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
         switch (opt) {
-        case 't': toroidal = true; break;
-        case 's': r_s = true; break;
-        case 'n': generation = atoi(optarg); break;
+            //set toroidal to true
+        case 't':
+            toroidal = true;
+            break;
+            //sets the cursers to false
+        case 's': r_s = false; break;
+        case 'n':
+            //sets the number of generations
+            generation = atoi(optarg);
+            //checks that it's a valud number of generation
+            if (generation < 1) {
+                fprintf(stderr, "Invalid number of generations");
+                //if you give an invalid number of generations, set the defult to 100
+                generation = 100;
+            }
+            break;
         case 'i':
+            //sets the input file
             infile = fopen(optarg, "r");
+            //if the input file is invalid, return an error
             if (infile == NULL) {
                 fprintf(stderr, "Error with the file");
+                exit(1);
             }
             break;
         case 'o':
+            //sets the outfile
             outfile = fopen(optarg, "w");
+            //if the outfile is invalid, return an error
             if (outfile == NULL) {
                 fprintf(stderr, "Error with the file");
+                exit(1);
             }
             break;
         default: fprintf(stderr, "Not a valid option. Use %s -[asctel]\n", argv[0]);
         }
     }
-
+    //read the file
     fscanf(infile, "%d %d\n", &rows, &cols);
+    //create universe a
     Universe *a = uv_create(rows, cols, toroidal);
+    //make sure that you have a valid memory
     if (a == NULL) {
-        fprintf(stderr, "Allocstion failed\n");
+        fprintf(stderr, "Allocation failed\n");
         return false;
     }
+    //create universe b
     Universe *b = uv_create(rows, cols, toroidal);
+    //make sure that you have a valid memory
     if (b == NULL) {
-        fprintf(stderr, "Allocstion failed\n");
+        fprintf(stderr, "Allocation failed\n");
         return false;
     }
     uv_populate(a, infile);
-
+    //counter that will be used to check the 3 conditions to see if a cell is alive/dead
     int count = 0;
-    for (int i = 0; i > generation; i++) {
-        for (int r = 0; r > rows; r++) {
-            printf("\n");
-            for (int c = 0; c > cols; c++) {
+    //sets the screen for curses
+    initscr();
+    noecho();
+    curs_set(FALSE);
+    //generate through the generations
+    for (int i = 0; i < generation; i++) {
+        //generate through the rows
+        for (int r = 0; r < rows; r++) {
+            //generate through the cols
+            for (int c = 0; c < cols; c++) {
+                //set our counter to the amount of neighboors in universe a
                 count = uv_census(a, r, c);
+                //if the cell has 3 or 2 neighboors, it stays alive
                 if (count == 2 || count == 3) {
                     uv_live_cell(b, r, c);
-                    if (r_s == false) {
+                    //does the ncurser
+                    if (r_s != false) {
+                        clear();
                         mvprintw(r, c, "o");
-                        // usleep(DELAY);
+                        refresh();
+                        usleep(DELAY);
                     }
                 }
                 bool x = uv_get_cell(a, r, c);
+                //if there is a dead cell with three neighboors, make the cell alive
                 if (x == false && count == 3) {
                     uv_live_cell(b, r, c);
-                    if (r_s == false) {
+                    //does the ncurses
+                    if (r_s != false) {
+                        clear();
                         mvprintw(r, c, "o");
-                        // usleep(DELAY);
+                        refresh();
+                        usleep(DELAY);
                     }
                 } else {
-                    if (count < 2) {
-                        uv_dead_cell(b, r, c);
-                        if (r_s == false) {
-                            mvprintw(r, c, "o");
-                            //   usleep(DELAY);
-                        }
-                    }
+                    //kill the cell
+                    uv_dead_cell(b, r, c);
                 }
+                //swaping the universes
+                Universe *temp = a;
+                a = b;
+                b = temp;
             }
         }
-        count = 0;
-        Universe *temp = b;
-        a = b;
-        b = temp;
-        clear();
-        refresh();
-        usleep(DELAY);
     }
+    //stop the curser
     endwin();
+    //print a
     uv_print(a, outfile);
+    //delete universe b
     uv_delete(b);
+    //delete universe a
     uv_delete(a);
+    return 0;
 }
--- asgn3/universe.c
+++ asgn3/universe.c
@@ -7,9 +7,10 @@ struct Universe {
     bool **grid;
     bool toroidal;
 };
-
+//allocates memory for the universes
 Universe *uv_create(int rows, int cols, bool toroidal) {
     Universe *u = (Universe *) calloc(1, sizeof(Universe));
+    //if we are in an invalid pointer; return Null
     if (u == NULL) {
         fprintf(stderr, "Allocation failed\n");
         return NULL;
@@ -17,56 +18,62 @@ Universe *uv_create(int rows, int cols, bool toroidal) {
     u->rows = rows;
     u->cols = cols;
     u->toroidal = toroidal;
+    //allocate memory for the grid
     u->grid = (bool **) calloc(rows, sizeof(bool *));
+    //if we are in an invalid location, return n
     if (u == NULL) {
         fprintf(stderr, "Allocation failed\n");
         return NULL;
     }
+    // allocate memory for the value of the grid
     for (int i = 0; i < rows; i++) {
-        if (u == NULL) {
-            fprintf(stderr, "Allocation failed\n");
-            return NULL;
-        }
         u->grid[i] = (bool *) calloc(cols, sizeof(bool));
     }
+    //return a universe
     return u;
 }
+//free the memory I allocated
 void uv_delete(Universe *u) {
+    //free the memory for the value of the grid
     for (int i = 0; i < u->rows; i++) {
         free(u->grid[i]);
     }
+    //free the memeory of the grid
     free(u->grid);
+    //free the universe
     free(u);
     return;
 }
-
+//returns the number of rows in a universe
 int uv_rows(Universe *u) {
     return u->rows;
 }
-
+//returns the numbr of cols in a universe
 int uv_cols(Universe *u) {
     return u->cols;
 }
-
+//if pointer or c are are >= the number of rows or cols and if pointer r or c <0,
+//then it's out of bounce and return false, else return true
 bool out_of_bounds(Universe *u, int r, int c) {
-    if (r > u->rows || r < 0 || c > u->rows || c < 0) {
+    if (r >= u->rows || r < 0 || c >= u->rows || c < 0) {
         return false;
     } else {
         return true;
     }
 }
-
+//if the pointer is in bounce, set the grid to true else nothing
 void uv_live_cell(Universe *u, int r, int c) {
-    int x = out_of_bounds(u, r, c);
+    bool x = out_of_bounds(u, r, c);
     if (x == true) {
         u->grid[r][c] = true;
+        return;
     } else {
         return;
     }
 }
-
+//if the  pointer is in bounce, set the grid to false, else nothing
 void uv_dead_cell(Universe *u, int r, int c) {
-    int x = out_of_bounds(u, r, c);
+    bool x = out_of_bounds(u, r, c);
     if (x == true) {
         u->grid[r][c] = false;
         return;
@@ -74,41 +81,59 @@ void uv_dead_cell(Universe *u, int r, int c) {
         return;
     }
 }
-
+//checks if a cell is dead/alive
 bool uv_get_cell(Universe *u, int r, int c) {
-    int x = out_of_bounds(u, r, c);
-    if (x == false) {
-        return false;
+    //check if the cell is in bound
+    bool x = out_of_bounds(u, r, c);
+    //check if we are in toroidal
+    if (u->toroidal != false) {
+        //if its out of bounce and the cell is dead, return false
+        if (x == false || u->grid[r][c] == false) {
+            return false;
+        }
+        //if its in bound and the cell is alive, return true
+        if (x == true || u->grid[r][c] == true) {
+            return true;
+        }
+        //if not in toroidal, return the value of the cell
     } else {
         return u->grid[r][c];
     }
+    return u->grid[r][c];
 }
-
+//populate the universe
 bool uv_populate(Universe *u, FILE *infile) {
     int row = 0;
     int colum = 0;
+    //if the first file is scanned for the dimention
     bool dim = false;
+    //scan the file until you reach the end
     while (fscanf(infile, "%d %d\n", &row, &colum) != EOF) {
+        //we read the dimentions
         dim = true;
+        //if it's out f bounce failed to populate
         int x = out_of_bounds(u, row, colum);
         if (x == false && dim == true) {
             return false;
             fprintf(stderr, "Failed to populate\n");
+            //else populate the grid
         } else {
             u->grid[row][colum] = true;
         }
     }
-    return false;
+    return true;
 }
-
+//counts the neighboors
 int uv_census(Universe *u, int r, int c) {
     int neighboors = 0;
+    //if not in toroidal, then don't do modulo and loop around (r,c)
     if (u->toroidal == false) {
         if (uv_get_cell(u, r - 1, c - 1) == true) {
             neighboors += 1;
         }
         if (uv_get_cell(u, r - 1, c) == true) {
             neighboors += 1;
+            printf("second one %d", neighboors);
         }
         if (uv_get_cell(u, r - 1, c + 1) == true) {
             neighboors += 1;
@@ -129,11 +154,10 @@ int uv_census(Universe *u, int r, int c) {
         if (uv_get_cell(u, r + 1, c + 1) == true) {
             neighboors += 1;
         }
+        //if in toroidal, do module to make sure there is no erros with %
     } else {
         int r_1 = ((r - 1) + u->rows) % u->rows;
         int c_1 = ((c - 1) + u->cols) % u->cols;
-        //  int r_plus_1 = ((r + 1) + u->rows - 1) % u->rows;
-        // int c_plus_1 = ((c + 1) + u->cols - 1) % u->cols;
         if (uv_get_cell(u, r_1, c_1) == true) {
             neighboors += 1;
         }
@@ -160,22 +184,26 @@ int uv_census(Universe *u, int r, int c) {
             neighboors += 1;
         }
     }
+    //return the number of neighboors
     return neighboors;
 }
+//print a universe
 void uv_print(Universe *u, FILE *outfile) {
+    //iterate through the rows
     for (int i = 0; i < u->rows; i++) {
+        //iterate through the cols
         for (int t = 0; t < u->cols; t++) {
+            //if the cell is alive, print an o
             if (u->grid[i][t] == true) {
                 fprintf(outfile, "%s", "o");
-            } else {
+            }
+            //if it is dead, print a .
+            if (u->grid[i][t] == false) {
                 fprintf(outfile, "%s", ".");
             }
-            // 	fprintf("\n");
-            //	fprintf(outfile, "%s", "\n");
         }
+        //print a new line
         fprintf(outfile, "%s", "\n");
     }
-    //	fprintf("\n");
-    //	fprintf(outfile, "%s", "\n");
     return;
 }
