73524d813118e93f680a6a99fd48807e60124408
kathy <cduarter@ucsc.edu>
2021-02-15 08:59:10 -0800

    small () fix

--- asgn5/quick.c
+++ asgn5/quick.c
@@ -5,42 +5,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int64_t particion(uint32_t *a, int64_t lo, uint64_t hi) {
+uint32_t compares;
+uint32_t moves;
+
+int64_t partition(uint32_t *a, int64_t lo, uint64_t hi) {
+    //  int64_t x = (hi - lo ) / 2;
     int64_t pivot = a[lo + ((hi - lo) / 2)];
+
     int64_t i = lo - 1;
     int64_t j = hi + 1;
-
     do {
         i += 1;
         while (a[i] < pivot) {
+            compares += 1;
             i += 1;
         }
         j -= 1;
         while (a[j] > pivot) {
+            compares += 1;
+
             j -= 1;
         }
         if (i < j) {
-            uint32_t temp = a[j];
-            a[j] = a[i];
-            a[i] = temp;
+            moves += 3;
+            uint32_t temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
         }
-
+        moves += 3;
     } while (i < j);
     return j;
 }
-
 void quick_sort(uint32_t *A, uint32_t n) {
     int64_t left = 0;
-    int64_t right = n - 1;
+    int64_t right = (n - 1);
+    ;
     Stack *s = stack_create();
+
     stack_push(s, left);
     stack_push(s, right);
 
-    while (!stack_empty(s)) {
-        stack_pop(s, &left);
-        stack_pop(s, &right);
-        uint32_t p = particion(A, left, right);
-        if (p + 1 < right) {
+    while (stack_empty(s) != true) {
+        int64_t hi = 0;
+        hi = stack_pop(s, &right);
+        int64_t lo = 0;
+        lo = stack_pop(s, &left);
+        uint32_t p = partition(A, left, right);
+        if ((p + 1) < right) {
             stack_push(s, p + 1);
             stack_push(s, right);
         }
