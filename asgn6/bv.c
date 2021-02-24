9c52f3e08bf7a0a1591907849f2d6a6d8492d005
kathy <cduarter@ucsc.edu>
2021-02-24 14:37:53 -0800

    v3: fixed errors after trying the proffesor's test cases

--- asgn6/bv.c
+++ asgn6/bv.c
@@ -12,8 +12,8 @@ BitVector *bv_create(uint32_t length) {
     BitVector *v = (BitVector *) calloc(1, sizeof(BitVector));
     if (v) {
         v->length = length;
-        uint32_t num_byte = (length / 8 == 0) + (length % 8 ? 1 : 0);
-        v->vector = (uint8_t *) calloc(num_byte, sizeof(uint8_t));
+        uint32_t num_byte = length % 8 == 0 ? length / 8 : (length / 8) + 1;
+        v->vector = calloc(num_byte, sizeof(uint8_t));
         return v;
     } else {
         return NULL;
@@ -28,32 +28,27 @@ void bv_delete(BitVector **bv) {
 }
 
 uint32_t bv_length(BitVector *bv) {
-    printf("length: %d\n", bv->length);
     return bv->length;
 }
 
 void bv_set_bit(BitVector *bv, uint32_t i) {
-    bv->vector[i / 8] |= 1 << (i % 8) / 8;
-    printf("setting a bit: %d\n", bv->vector[i / 8]);
+    bv->vector[i / 8] |= 1 << (i % 8);
     return;
 }
 
 void bv_clr_bit(BitVector *bv, uint32_t i) {
-    uint32_t mask = ~(1 << (i % 8) / 8);
-    bv->vector[i / 8] = bv->vector[i / 8] & mask;
+    bv->vector[i / 8] = bv->vector[i / 8] & ~(1 << (i % 8));
     return;
 }
 
 uint8_t bv_get_bit(BitVector *bv, uint32_t i) {
-    uint32_t mask = 1 << (i % 8) / 8;
-    bv->vector[i / 8] = bv->vector[i / 8] & mask;
-    return bv->vector[i / 8] = bv->vector[i / 8] >> (i % 8) / 8;
+    return (bv->vector[i / 8] >> (i % 8)) & 1;
 }
 
 void bv_print(BitVector *bv) {
-    for (uint32_t i = 0; i < bv->length; i += 1) {
+    for (uint32_t i = 0; i < bv_length(bv); i += 1) {
         uint32_t x = bv_get_bit(bv, i);
-        printf("%d\t", x);
+        printf("%d", x);
     }
     printf("\n");
 }
