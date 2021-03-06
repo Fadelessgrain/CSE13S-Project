627a2fb955b91459c262b766e78621bb028ca9b4
kathy <cduarter@ucsc.edu>
2021-03-06 13:57:19 -0800

    init

---
+++ asgn7/decode.c
@@ -0,0 +1,13 @@
+#include "code.h"
+#include "endian.h"
+#include "io.h"
+#include "math.h"
+#include "word.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("hello");
+    return 0;
+}
