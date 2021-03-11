7f6e78d795f29fb1451add91887d0d5c6ca4f73d
kathy <cduarter@ucsc.edu>
2021-03-10 21:40:38 -0800

    fixed some issues after testing + attending gabe's section

--- asgn7/word.c
+++ asgn7/word.c
@@ -3,33 +3,49 @@
 #include "code.h"
 
 #include <stdlib.h>
+#include <string.h>
 
 Word *word_create(uint8_t *syms, uint32_t len) {
     Word *w = (Word *) malloc(sizeof(Word));
     if (!w) {
         return NULL;
     }
-    w->syms = syms;
     w->len = len;
+    w->syms = (uint8_t *) malloc(len * sizeof(uint8_t));
+    if (!w->syms) {
+        return NULL;
+    }
+    memcpy(w->syms, syms, len);
     return w;
 }
 
 Word *word_append_sym(Word *w, uint8_t sym) {
-    Word *nw = (Word *) malloc(sizeof(Word));
-    nw->syms = calloc(w->len + 1, sizeof(uint8_t));
-    for (uint8_t i = 0; i < w->len; i += 1) {
-        nw->syms[i] = w->syms[i];
+    if (!w) {
+        return NULL;
     }
-    nw->syms[w->len] = sym;
+    Word *nw = (Word *) malloc(sizeof(Word));
+    if (nw) {
+        nw->len = w->len + 1;
+        nw->syms = (uint8_t *) malloc((nw->len) * sizeof(uint8_t));
+        if (nw->syms) {
+            for (uint8_t i = 0; i < w->len; i += 1) {
+                nw->syms[i] = w->syms[i];
+            }
+            nw->syms[w->len] = sym;
+        }
+    }
     return nw;
 }
 
 void word_delete(Word *w) {
+    free(w->syms);
+    w->syms = NULL;
     free(w);
+    w = NULL;
 }
 
 WordTable *wt_create(void) {
-    WordTable *wt = (WordTable *) malloc(sizeof(MAX_CODE));
+    WordTable *wt = (WordTable *) calloc(MAX_CODE, sizeof(Word));
     if (wt) {
         wt[EMPTY_CODE] = word_create(NULL, 0);
     }
@@ -37,9 +53,12 @@ WordTable *wt_create(void) {
 }
 
 void wt_reset(WordTable *wt) {
-    for (int i = START_CODE; i < MAX_CODE; i += 1) {
-        if (i != EMPTY_CODE) {
-            wt_delete(&wt[i]);
+    if (wt) {
+        for (int i = START_CODE; i < MAX_CODE; i += 1) {
+            if (wt[i]) {
+                word_delete(wt[i]);
+                wt[i] = NULL;
+            }
         }
     }
     return;
@@ -49,7 +68,8 @@ void wt_delete(WordTable *wt) {
     if (wt) {
         for (int i = 0; i < MAX_CODE; i += 1) {
             if (wt[i]) {
-                wt_delete(&wt[i]);
+                word_delete(wt[i]);
+                wt[i] = NULL;
             }
         }
 
