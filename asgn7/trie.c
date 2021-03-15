461594917e679d9f2c44b7f0c5072ae612be5c39
kathy <cduarter@ucsc.edu>
2021-03-14 18:29:56 -0700

    added comments

--- asgn7/trie.c
+++ asgn7/trie.c
@@ -5,11 +5,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// creates a new trie + allocates memory for it
 TrieNode *trie_node_create(uint16_t code) {
     TrieNode *n = (TrieNode *) malloc(sizeof(TrieNode));
     if (!n) {
         return NULL;
     }
+    // sets the children to null
     for (int i = 0; i < ALPHABET; i += 1) {
         n->children[i] = NULL;
     }
@@ -17,6 +19,7 @@ TrieNode *trie_node_create(uint16_t code) {
     return n;
 }
 
+// frees the memory of a trie
 void trie_node_delete(TrieNode *n) {
     if (n) {
         free(n);
@@ -25,10 +28,12 @@ void trie_node_delete(TrieNode *n) {
     }
 }
 
+// creates an emty trie
 TrieNode *trie_create(void) {
     return trie_node_create(EMPTY_CODE);
 }
 
+// restes the root/chidlren of a trie w/out reseting the parent
 void trie_reset(TrieNode *root) {
     for (int i = 0; i < ALPHABET; i += 1) {
         if (root->children[i]) {
@@ -39,6 +44,7 @@ void trie_reset(TrieNode *root) {
     return;
 }
 
+// deletes a trie + its chidlren
 void trie_delete(TrieNode *n) {
     if (n) {
         for (int i = 0; i < ALPHABET; i += 1) {
@@ -50,6 +56,7 @@ void trie_delete(TrieNode *n) {
     return;
 }
 
+// check if a sym is in the trie struct
 TrieNode *trie_step(TrieNode *n, uint8_t sym) {
     if (n->children[sym] == NULL) {
         return NULL;
